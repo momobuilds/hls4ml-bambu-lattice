c6e305761919412153f3bcbc00cac3c2ed9da4a5
Andrea Oggioni <24810621+etabeta1@users.noreply.github.com>
2026-02-10 21:22:21 +0100

    Add bambu backend in tests

--- test/pytest/test_pooling.py
+++ test/pytest/test_pooling.py
@@ -33,7 +33,7 @@ def keras_model_1d(request):
     return model, model_type, pads, strides
 
 
-@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'Catapult', 'oneAPI'])
+@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'Catapult', 'oneAPI', 'Bambu'])
 @pytest.mark.parametrize(
     'keras_model_1d',
     [
@@ -78,7 +78,7 @@ def test_pool1d(backend, keras_model_1d, data_1d, io_type):
     np.testing.assert_allclose(y_keras, y_hls, rtol=0, atol=atol, verbose=True)
 
 
-@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'oneAPI'])
+@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'oneAPI', 'Bambu'])
 @pytest.mark.parametrize(
     'keras_model_1d',
     [
@@ -130,7 +130,7 @@ def keras_model_2d(request):
     return model, model_type, pads, strides
 
 
-@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'Catapult', 'oneAPI'])
+@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'Catapult', 'oneAPI', 'Bambu'])
 @pytest.mark.parametrize(
     'keras_model_2d',
     [
@@ -173,7 +173,7 @@ def test_pool2d(backend, keras_model_2d, data_2d, io_type):
     np.testing.assert_allclose(y_keras, y_hls, rtol=0, atol=atol, verbose=True)
 
 
-@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'oneAPI'])
+@pytest.mark.parametrize('backend', ['Quartus', 'Vitis', 'Vivado', 'oneAPI', 'Bambu'])
 @pytest.mark.parametrize(
     'keras_model_2d',
     [
